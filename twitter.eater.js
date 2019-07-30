fce5f9815149da74e5e27569fa3f3a5fb5e22632
Kim Nguyen <kimnguyen@admins-MacBook-Pro.local>
2019-07-30 07:51:15 -0700

    Add twitter widgets

--- twitter.eater.js
+++ twitter.eater.js
@@ -12,11 +12,13 @@ const
 var 
 
   Twitter           = require('twitter'),
+  creds              = require('./creds.js');
   tclient           = new Twitter({
-      consumer_key: '',
-      consumer_secret: '',
-      access_token_key: '',
-      access_token_secret: ''}),
+    consumer_key: creds.consumer_key,
+    consumer_secret: creds.consumer_secret,
+    access_token_key: creds.access_token_key,
+    access_token_secret: creds.access_token_secret
+  }),
   stream            = tclient.stream('statuses/filter', {track: 'trump'});
 
 
@@ -27,51 +29,52 @@ stream.on('data', function(tweet) {
 let perSec = 0; 
 
 stream.on('data', function (tw) {             
-  "use strict";
-  perSec += 1;
-  if (tw.entities != undefined) {                              
-    let 
-      tweetEntities = tw.entities,                        
-      mentions = tweetEntities.user_mentions
-        .map((mention) => mention.screen_name)  // extract mentions
-        .join(','),                             // results in a comma delimited list of screen names mentioned
-      urls = tweetEntities.urls           
-        .map((aUrlEntity) => aUrlEntity.url)    // extract URL
-        .join(',');                             // results in a comma delimited list of URLs
+    "use strict";
+    perSec += 1;
+    if (tw.entities != undefined) {                              
+        let 
+            tweetEntities = tw.entities,                        
+            mentions = tweetEntities.user_mentions
+              .map((mention) => mention.screen_name)  // extract mentions
+              .join(','),                             // results in a comma delimited list of screen names mentioned
+            urls = tweetEntities.urls           
+              .map((aUrlEntity) => aUrlEntity.url)    // extract URL
+              .join(',');                             // results in a comma delimited list of URLs
 
-    client.xadd('tweets',                       // add to the stream `tweets`
-      '*',                                      // at the latest sequence
-      'id',tw.id_str,                           // stream field `id` with the tweet id (in string format because JS is bad with big numbers!)
-      'screen_name',tw.user.screen_name,        // stream field `screen_name` with the twitter screen name
-                                                // stream field `text` with either the extended (>140 chars), if present, or normal if (<140 chars)
-      'text',(tw.extended_tweet && tw.extended_tweet.full_text) ? tw.extended_tweet.full_text : tw.text,
-      'mentions',mentions,                      // stream field `mentions` with the mentions comma delimited list
-      'urls',urls,                              // stream field `urls` with urls comma delimited list
-      function(err) {
-          if (err) { throw err; }               // handle any errors - a production service would need better error handling.
-      }
-    );
-  }
+        client.xadd('tweets',                         // add to the stream `tweets`
+            '*',                                      // at the latest sequence
+            'id',tw.id_str,                           // stream field `id` with the tweet id (in string format because JS is bad with big numbers!)
+            'icon',tw.user.profile_image_url,                           // stream field `id` with the tweet id (in string format because JS is bad with big numbers!)
+            'screen_name',tw.user.screen_name,        // stream field `screen_name` with the twitter screen name
+                                                      // stream field `text` with either the extended (>140 chars), if present, or normal if (<140 chars)
+            'text',(tw.extended_tweet && tw.extended_tweet.full_text) ? tw.extended_tweet.full_text : tw.text,
+            'mentions',mentions,                      // stream field `mentions` with the mentions comma delimited list
+            'urls',urls,                              // stream field `urls` with urls comma delimited list
+            function(err) {
+                if (err) { throw err; }               // handle any errors - a production service would need better error handling.
+            }
+        );
+    }
 });
 
 setInterval(function() {                    
-  "use strict";
-  console.log('Tweets per/sec',perSec);
-  perSec = 0;                                
+    "use strict";
+    console.log('Tweets per/sec',perSec);
+    perSec = 0;                                
 },1000); 
 
 stream.on('error', function (err) {          
-  "use strict";
-  console.error('Twitter Error', err);       
+    "use strict";
+    console.error('Twitter Error', err);       
 });                                          
                                              
 
-let elementProcessors = {                        // Element processor pattern. This listens to stream (control-plane-eater) with `xread` for a events
-  'tweets'   : (element) => function(done) {     // the element is the output from redis
-    let 
-      dataObj = _(element[1]).chunk(2).fromPairs().value(); // grab the data which is in interleaved array format (field, value, field value, ....) and convert to pairs and create an object out of it.
-    done();                                      // note that we're done and we can listen again.
-  }
+let elementProcessors = {                             // Element processor pattern. This listens to stream (control-plane-eater) with `xread` for a events
+    'tweets'   : (element) => function(done) {        // the element is the output from redis
+        let 
+            dataObj = _(element[1]).chunk(2).fromPairs().value(); // grab the data which is in interleaved array format (field, value, field value, ....) and convert to pairs and create an object out of it.
+        done();                                       // note that we're done and we can listen again.
+    }
 };
 
 streamProcessor(controlPlaneClient,Object.keys(elementProcessors),elementProcessors);
